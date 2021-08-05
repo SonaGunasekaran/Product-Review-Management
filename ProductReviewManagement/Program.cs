b909ec3efd4462944d4b1ce592e1ee4bc26c18d8
[Sona] <[sona16061999@gmail.com]>
2021-08-05 20:15:17 +0530

    [Sona] Retrieve Top Three Rated Products

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -8,7 +8,19 @@ namespace ProductReviewManagement
         {
             Console.WriteLine("Product Review Management!");
             ReviewManager review = new ReviewManager();
-            review.AddReviews();
+            Console.WriteLine("1.Add Product Reviews\n2.Retrieve Top three Rated Products");
+            Console.WriteLine("Enter the option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    review.AddReviews();
+                    break;
+                case 2:
+                    review.RetrieveTopThreeRated();
+                    break;
+
+            }
         }
     }
 }
